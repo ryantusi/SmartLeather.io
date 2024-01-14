2c4540c288039b658f92807fbdfe9912f4572763
Ryan Tusi <ryantusi45@gmail.com>
2024-01-14 18:17:07 +0530

    Main App

---
+++ app.py
@@ -0,0 +1,25 @@
+from cs50 import SQL
+from flask import Flask, redirect, render_template, request, session
+from flask_session import Session
+
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/demo")
+def demo():
+    return render_template("demo.html")
